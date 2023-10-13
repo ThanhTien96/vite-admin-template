4c3b95045dff40eae36acae184c9269f2f8367c7
ThanhTien <thanhtien2094@gmail.com>
2023-10-13 11:25:59 +0700

    delete gaugeBox

--- src/pages/HomePage/partials/index.tsx
+++ src/pages/HomePage/partials/index.tsx
@@ -2,4 +2,3 @@ export {default as TinyChart} from './TinyChart';
 export {default as ColumnChart} from './ColumnChart';
 export {default as PieChart} from './PieChart';
 export {default as WatchTable} from './WhatchTable';
-export {default as GaugeBox} from './GaugeBox';
